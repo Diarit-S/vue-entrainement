3269739bd90007858e74360c35f7f50aa3720845
Diarit-S <diarit-94@hotmail.fr>
2019-07-23 21:22:44 +0200

    continue the lesson

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,38 @@
 <template>
-  <div id="app">
-    <button class="btn btn-primary m-5" @click="display=!display">toggle</button>
-    <div class="d-flex flex-column justify-content-center align-items-center">
-      <transition enter-active-class="animated bounceIn">
-        <div v-if="display" class="alert alert-danger w-50">C'est un alerte !</div>
-      </transition>
-    </div>
+  <div id="app" class="container p-5">
+	<app-add-user></app-add-user>
+	<br /><br />
+	<app-user-list :users="users" ></app-user-list>
   </div>
 </template>
 
 <script>
-
-
+import AppAddUser from './components/AddUser';
+import AppUserList from './components/UserList';
 export default {
   name: 'app',
- 
-  data(){
-    return {
-      display : false,
-    }
+  components: {
+	AppAddUser,
+	AppUserList
   },
+  data() {
+	return {
+  	  users: {}
+	}
+  },
+  created() {
+    this.$http.get('user.json', {
+      headers: {
+        'hello': '123'
+      }
+    })
+      .then(res => {
+        console.log(res);
+        this.users = res.data;
+      })
+      .catch(err => console.log(err))
+  }
 }
 </script>
-
-<style scoped>
-  .v-enter {
-    opacity: 0;
-    transform: translateX(-20px);
-    filter: grayscale(1);
-  }
-
-  .v-enter-to {
-
-  }
-
-  .v-enter-active {
-    transition : all 0.5s ease-in-out;
-  }
-
-  .v-leave {
-    
-  }
-  .v-leave-to {
-    opacity: 0;
-    transform: translateX(-20px);
-    filter: grayscale(1);
-  }
-  .v-leave-active {
-    transition : all 0.5s ease-in-out;
-  }
-
-
+<style>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://vue-entrainement.firebaseio.com/';
+axios.interceptors.request.use((req)=>{
+  console.log({ req });
+  return req;
+})
+axios.interceptors.response.use((res)=>{
+  console.log({ res });
+  return res;
+})
+
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
 
